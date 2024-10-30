033a012a9f6a27f05309bd695f42d9ed13b8d933
paul-rl <prl7.dev@gmail.com>
2024-10-30 17:28:00 -0400

    Updated background color

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,7 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
+        background: "#241C33",
         foreground: "var(--foreground)",
       },
     },
