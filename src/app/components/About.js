940cd849e2473f17e1b4b7ed48f07f706381033f
paul-rl <prl7.dev@gmail.com>
2024-10-30 21:17:41 -0400

    Fixed black text on some browsers

--- src/app/components/About.js
+++ src/app/components/About.js
@@ -2,8 +2,8 @@ import { STRINGS } from "../constants/strings";
 
 export default function About() {
   return (
-    <div>
-        <p className="mt-2">{STRINGS.about.intro}</p>
+    <div className="text-gray-300">
+        <p className="mt-2 ">{STRINGS.about.intro}</p>
         <p className="mt-2">{STRINGS.about.school}</p>
         <p className="mt-2">{STRINGS.about.internship}</p>
         <p className="mt-2">{STRINGS.about.freetime}</p>
